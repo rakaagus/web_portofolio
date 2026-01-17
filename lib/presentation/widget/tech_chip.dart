3fc547028ff735b94c2f32cc4c2d3d36f33e2f19
rakaagus <kakakunyukya@gmail.com>
2026-01-17 23:49:39 +0700

    [Feat] add experience section & add new widget

---
+++ lib/presentation/widget/tech_chip.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+
+class TechChip extends StatelessWidget {
+
+  String label;
+  Color color;
+
+  TechChip({
+    required this.label,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: color.withOpacity(0.2)),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          color: color,
+          fontSize: 12,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
